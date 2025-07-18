6e22fa03f2fb9dda4ac4a2b66c3f121ce7725bac
imran <imranfaraji880@gmail.com>
2025-07-19 00:17:25 +0600

    try to deploy on vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     
-    await client.connect();
+    
 
     
     const foodsCollection=client.db('foodShareCollection').collection('foods')
@@ -44,7 +44,7 @@ async function run() {
       res.send(result)
     })
     
-    await client.db("admin").command({ ping: 1 });
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     
