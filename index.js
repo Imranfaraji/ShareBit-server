3a60623bd3c8221e88869e116ea9b17546487acd
imran <imranfaraji880@gmail.com>
2025-07-18 15:55:55 +0600

    connet database

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors=require('cors')
+const app = express()
+const port = 3000
+
+// middleware
+
+app.use=(cors())
+app.use=(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pztqlyl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    
+    await client.connect();
+
+    
+    const foodsCollection=client.db('foodShareCollection').collection('foods')
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
